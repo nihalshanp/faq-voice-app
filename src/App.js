0ca382511f8084d98750f1a878088dcfd9821480
Nihal shanp <pnihalshan94@gmail.com>
2021-05-05 14:04:09 +0530

    created scrolling feature

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ChakraProvider, theme } from '@chakra-ui/react';
 import NavBar from './Components/NavBar';
 import Faq from './Components/Faq/Faq';
 import alanBtn from "@alan-ai/alan-sdk-web";
-
+import {scroller} from "react-scroll"
 const App = () => {
   const alanBtnInstance = useRef(null);
   const [index, setIndex] = useState(null)
@@ -14,6 +14,11 @@ useEffect(() => {
             key: '23940fa1cd518889d176c4455b7164532e956eca572e1d8b807a3e2338fdd0dc/stage',
             onCommand: (commandData) => {
                 if (commandData.command === 'gotoFaq') {
+                  scroller.scrollTo(`accordion-item${commandData.faqId}`,{
+                    duration: 800,
+                    delay: 0,
+                    smoot: 'easeInOutQuart'
+                  })
                   setIndex(commandData.faqId -1)
                 }
                 }
--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -20,7 +20,7 @@ const Faq = ({index, setIndex}) => {
       </Box>
       <Accordion allowToggle index={index}>
         {FAQ_LIST.map(faq => (
-          <AccordionItem key={faq.id}>
+          <AccordionItem key={faq.id} name={`accordion-item${faq.id}`}>
             <AccordionButton onClick={() => setIndex(faq.id - 1)}>
               <Box flex="1" textAlign="left">
                 <Text fontWeight="semibold">{faq.question}</Text>
