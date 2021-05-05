5c48354207ba5d685eec3657cd9bf5303cf9e762
Nihal shanp <pnihalshan94@gmail.com>
2021-05-05 13:43:38 +0530

    created sending voice data to React app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import NavBar from './Components/NavBar';
 import Faq from './Components/Faq/Faq';
@@ -6,22 +6,23 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 
 const App = () => {
   const alanBtnInstance = useRef(null);
+  const [index, setIndex] = useState(null)
 
 useEffect(() => {
     if (!alanBtnInstance.current) {
         alanBtnInstance.current = alanBtn({
             key: '23940fa1cd518889d176c4455b7164532e956eca572e1d8b807a3e2338fdd0dc/stage',
             onCommand: (commandData) => {
-                if (commandData.command === 'go:back') {
-                        // Call the client code that will react to the received command
-                    }
+                if (commandData.command === 'gotoFaq') {
+                  setIndex(commandData.faqId -1)
+                }
                 }
         });
     }
 }, []);
   return <ChakraProvider theme={theme}>
     <NavBar/>
-    <Faq/>
+    <Faq index={index} setIndex={setIndex} />
   </ChakraProvider>;
 };
 
--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -12,16 +12,16 @@ import {
 } from '@chakra-ui/react';
 import FAQ_LIST from './Faq.json';
 
-const Faq = () => {
+const Faq = ({index, setIndex}) => {
   return (
     <Flex direction="column" p="4">
       <Box mb="8">
         <Heading size="md">Frequently Asked Questions</Heading>
       </Box>
-      <Accordion>
+      <Accordion allowToggle index={index}>
         {FAQ_LIST.map(faq => (
           <AccordionItem key={faq.id}>
-            <AccordionButton>
+            <AccordionButton onClick={() => setIndex(faq.id - 1)}>
               <Box flex="1" textAlign="left">
                 <Text fontWeight="semibold">{faq.question}</Text>
               </Box>
