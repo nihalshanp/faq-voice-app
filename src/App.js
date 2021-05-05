b125a815e4f3b05e9ddafef5f739101c2023eca4
Nihal shanp <pnihalshan94@gmail.com>
2021-05-05 13:31:41 +0530

    Integrated alen ai

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import NavBar from './Components/NavBar';
 import Faq from './Components/Faq/Faq';
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 const App = () => {
+  const alanBtnInstance = useRef(null);
+
+useEffect(() => {
+    if (!alanBtnInstance.current) {
+        alanBtnInstance.current = alanBtn({
+            key: '23940fa1cd518889d176c4455b7164532e956eca572e1d8b807a3e2338fdd0dc/stage',
+            onCommand: (commandData) => {
+                if (commandData.command === 'go:back') {
+                        // Call the client code that will react to the received command
+                    }
+                }
+        });
+    }
+}, []);
   return <ChakraProvider theme={theme}>
     <NavBar/>
     <Faq/>
